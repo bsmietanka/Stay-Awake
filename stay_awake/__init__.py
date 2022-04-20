0866732e637bb0d6ae07c96929178c124c0ab7e3
bsmietanka <bsmietanka@users.noreply.github.com>
2022-04-20 16:02:43 +0200

    Fix bug with parser

--- stay_awake/__init__.py
+++ stay_awake/__init__.py
@@ -11,6 +11,7 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("interval", type=int, default=3,
                         help="Mouse movement interval in minutes")
+    args = parser.parse_args()
     while(True):
         sleep(args.interval * 60)
         pyautogui.moveTo(random.randint(0, 100),
