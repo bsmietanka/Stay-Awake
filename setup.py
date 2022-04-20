db3e3bb89c7c670cb01fb4d7012cb6107c62fbb1
bsmietanka <bsmietanka@users.noreply.github.com>
2022-04-20 15:45:01 +0200

    Package setup

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='stay-awake',
+    version='0.1.0',
+    description='Stay awake utility script',
+    author='Bartek Śmietanka',
+    author_email='bartek.smietanka@gmail.com',
+    packages=find_packages(include=['src']),
+    install_requires=[
+        'pyautogui',
+    ],
+    entry_points={
+        'console_scripts': ['stay_awake=src:main']
+    }
+)
