0b1428d6030c34367613e773de55bbd82e792272
bsmietanka <bsmietanka@users.noreply.github.com>
2022-04-21 14:40:51 +0200

    Add shift key press

--- stay_awake/__init__.py
+++ stay_awake/__init__.py
@@ -13,10 +13,12 @@ def main():
                         help="Mouse movement interval in minutes")
     args = parser.parse_args()
     assert args.interval > 0, "Interval must be positive"
+    print(f"System activity at {args.interval} minutes intervals")
     while(True):
         sleep(args.interval * 60)
         pyautogui.moveTo(random.randint(0, 100),
                          random.randint(0, 100))
+        pyautogui.press("shift")
         print("Mouse activity at {}".format(datetime.now().time()))
 
 if __name__ == "__main__":
